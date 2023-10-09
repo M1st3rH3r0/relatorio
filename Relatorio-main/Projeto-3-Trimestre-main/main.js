ef795a3e7230e211366d1ca6fc0a4b641484b4bd
Joao Mendes <110237276+M1st3rH3r0@users.noreply.github.com>
2023-10-09 12:13:29 -0300

    Add files via upload

---
+++ Relatorio-main/Projeto-3-Trimestre-main/main.js
@@ -0,0 +1,79 @@
+function TocarSomPom(){
+    document.querySelector("#som_tecla_pom").play();
+}
+
+
+
+function TocarSomA(){
+    document.querySelector("#som_tecla_A").play();
+}
+
+
+
+function TocarSomClap(){
+    document.querySelector("#som_tecla_clap").play();
+}
+
+
+
+function TocarSomTim(){ 
+    document.querySelector("#som_tecla_tim").play();
+}
+
+  
+
+
+function TocarSomPuff(){ 
+    document.querySelector("#som_tecla_puff").play();
+}
+
+
+
+function TocarSomSplash(){ 
+    document.querySelector("#som_tecla_splash").play();
+}
+
+
+
+function TocarSomToim(){ 
+    document.querySelector("#som_tecla_toim").play();
+}
+
+
+
+function TocarSomOoof(){ 
+    document.querySelector("#som_tecla_oof").play();
+}
+
+
+
+function TocarSomPsh(){ 
+    document.querySelector("#som_tecla_psh").play();
+}
+
+
+
+function TocarSomTic(){ 
+    document.querySelector("#som_tecla_tic").play();
+}
+
+
+
+
+function TocarSomTom(){ 
+    document.querySelector("#som_tecla_tom").play();
+}
+
+
+
+
+function TocarSomPipe(){ 
+    document.querySelector("#som_tecla_Pipe").play();
+}
+
+
+
+
+function TocarSomPare(){ 
+    document.querySelector("#som_tecla_pare").play();
+}
